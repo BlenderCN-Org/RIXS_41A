147be2e4c390ca2c9d11bf35bced7177e790bebc
JasonJungler <jasons10219@gmail.com>
2019-03-11 15:03:01 +0800

    new qtgraph test

--- TPS_BlueMagpie.py
+++ TPS_BlueMagpie.py
@@ -1,112 +1,168 @@
 import os, sys, time, random
-from PyQt5 import QtGui, QtCore
+from PyQt5 import QtGui, QtCore, QtWidgets
 from PyQt5.QtWidgets import (QApplication, QWidget, QMainWindow, QGridLayout, QFileDialog, QInputDialog, QAction,
                              QCheckBox, QGroupBox, QSpinBox, QProgressBar, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit,
                              QSpacerItem, QSizePolicy, QAction, QPushButton, qApp, QPlainTextEdit, QFormLayout, QAction,
                              QScrollBar,QSplitter,QTableWidgetItem,QTableWidget,QComboBox,QVBoxLayout,QGridLayout,
                              QPushButton, QMainWindow,QMessageBox,QLabel,QTextEdit,QProgressBar)
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QSize, Qt, QDate, QTime, QDateTime, QObject, QEvent, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import QSize, Qt, QDate, QTime, QDateTime, QObject, QEvent, pyqtSignal, QTimer, pyqtSlot
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
+import pyqtgraph as pg
+from pyqtgraph import PlotWidget
 import numpy as np
-from pyepics_device import Devices as ped
-from pyepics_tmp_device import TmpDevices as petd
-import epics as e
+import pandas as pd
 
 
+# Global Data
+
+# Test without class
+'''
+data = np.linspace(-5.0, 5.0, 10)
+data = pyqtSignal
+data.valueChanged.connect(ImageWidget.imgplot)
+'''
+
+    
+
+
+class Data(QWidget):
+    data = np.linspace(-5.0, 5.0, 10)
+    valueChanged = pyqtSignal(object)
+
+    def __init__(self, parent=None):
+        super(Data, self).__init__(parent)
+        self._t = self.data
+
+    @property
+    def t(self):
+        return self._t
+
+    @t.setter
+    def t(self, value):
+        self._t = value
+        self.valueChanged.emit(value)
+        print("Data emitted to plot.")
+        
+        #communicate with Command Widget
+        
+    @pyqtSlot(object)
+    def get_new_data(self, value):
+        self.t.setValue(value)
+        print("new data value received")
+
+    def make_cmd_connection(self, cmd_object):
+        cmd_object.valueChanged.connect(self.get_new_data)
+
+# Testing other dataset
+'''
+Y = np.linspace(-5.0, 5.0, 10)
+Z = (X,Y)
+imgdata = pd.DataFrame(X, Y)
+'''
 
 class RIXS(QMainWindow):
 
     def __init__(self):
         QMainWindow.__init__(self)
-        ###Window_size
-        self.setFixedSize(1300, 750)
-        ###Window_Title
+
+        # Window attributes
+
+        self.setFixedSize(2600, 1500)
         self.setWindowTitle('TPS blue magpie')
+
+
+        # Menu Bar
+
         exitAct = QAction(QIcon('exit.png'), ' &Quit', self)
-        ###Quit_button
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Exit application')
         exitAct.triggered.connect(qApp.quit)
-        ###Menu_Bar
+
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
+
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(exitAct)
-        ###EMCCD_button
+
         emccdMenu = menubar.addMenu("EMCCD")
         emccdMenu.addAction("cooling on")
         emccdMenu.addAction("cooling off")
-        ###SDD_button
-        sddMenu = menubar.addMenu('SDD')
-        ###Slit_button
-        slitMenu = menubar.addMenu('Slits')
-        ###Help_button
-        helpMenu = menubar.addMenu('Help')
 
+        '''To be continued'''
+        #sddMenu = menubar.addMenu('SDD')
+        #slitMenu = menubar.addMenu('Slits')
+        #helpMenu = menubar.addMenu('Help')
+
+        # Import panel
         self.panel_widget = Panel(self)
         self.setCentralWidget(self.panel_widget)
-
         self.show()
 
 
-###GUI_design
+
 class Panel(QWidget):
 
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
-        """
-        Import designed UI
-        """
-        self.initUI()
-    #layout
-    def initUI(self):
+
+
+        # Import designed UI
+        self.UI_layout()
+
+    def UI_layout(self):
         self.setFixedSize(1300, 750)
         self.setWindowTitle('TPS blue magpie')
 
-        ###Left_Column
+        # Left_Column
+
         hbox = QHBoxLayout(self)
         groupBox1 = QGroupBox()
         vbox = QVBoxLayout(groupBox1)
-        image_widget = ImagingWidget(self)
+        image_widget = ImageWidget(self)
         vbox.addWidget(image_widget, 1)
         command_widget=Command(self)
         vbox.addWidget(command_widget)
 
-        hbox.addWidget(groupBox1, 1)
 
-        ###Status_area
-        vbox2 = QVBoxLayout()
-        groupBox2 = QGroupBox("Experiment No.  1234;   PI: A. B. C. ", self)
-        vbox3 = QVBoxLayout(groupBox2)
+
+        # Right_Column
+
+        rightcolumn = QVBoxLayout()
+
+        status_box = QGroupBox("Experiment No.  1234;   PI: A. B. C. ", self)
+        status_contents = QVBoxLayout(status_box)
+
         flay = QFormLayout()
 
-        label1 = QLabel("Entarnce slit: 5 um", groupBox2)
-        flay.addRow(label1)
-        label2 = QLabel("AGM: 530 eV", groupBox2)
-        flay.addRow(label2)
-        label3 = QLabel("Exit slit: 50 um", groupBox2)
-        flay.addRow(label3)
-        label4 = QLabel("Sample: x=  , y=  , z=   , u=  , v=  , w=  , Ta=  , Tb=  K", groupBox2)
-        flay.addRow(label4)
-        label5 = QLabel("AGS: 530 eV", groupBox2)
-        flay.addRow(label5)
+        status = QLabel("Entrance slit: 5 um\n"
+                        "AGM: 530 eV\n"
+                        "Exit slit: 50 um\n"
+                        "Sample: x=  , y=  , z=   , u=  , v=  , w=  , Ta=  , Tb=  K\n"
+                        "AGS: 530 eV\n", status_box)
+        flay.addRow(status)
+        status_contents.addLayout(flay, 1)
+        # Real time update solution
+        '''
+        def update_label():
+        current_time = str(datetime.datetime.now().time())
+        ui.label.setText(current_time)
 
-        vbox3.addLayout(flay, 1)
-        vbox2.addWidget(groupBox2)
+        timer = QtCore.QTimer()
+        timer.timeout.connect(update_label)
+        timer.start(10000)  # every 10,000 milliseconds
+        '''
 
-        groupBox3 = QGroupBox()
+        spectrum_widget = SpectrumWidget(self)
 
-        vbox4 = QVBoxLayout(groupBox3)
-        m2 = SpectrumWidget(self)
-        vbox2.addWidget(m2)
-        hbox.addLayout(vbox2, 1)
+        rightcolumn.addWidget(status_box)
+        rightcolumn.addWidget(spectrum_widget)
 
-    ###Device_Object?
-    ###Data_list?
+        hbox.addWidget(groupBox1, 1)
+        hbox.addLayout(rightcolumn, 1)
 
         self.show()
 
@@ -120,8 +176,6 @@ class ImagingWidget(QWidget):
         self.layout().addWidget(self.toolbar)
         self.layout().addWidget(self.canvas)
 
-    def plot(self):
-        print("ok.")
 
 
 class SpectrumWidget(QWidget):
@@ -164,7 +218,6 @@ class ImagingCanvas(FigureCanvas):
 
         FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
-        ###Command.plot_image.connect(Command.plot)
         self.plot()
 
     # random image display
@@ -180,20 +233,24 @@ class ImagingCanvas(FigureCanvas):
         self.draw()
 
 class Command(QWidget):
+    valueChanged = pyqtSignal(object)
+
     def __init__(self, parent=None):
         super().__init__(parent)
+        self._v =  0
 
         # message
         self.command_message = QTextEdit(self)
-        welcome_message = '''
-        Welcome to TPS_BlueMagpie!
-        Type help to list commands.'''
+        time = QTime.currentTime()
+        welcome_message = ('<font color=blue>' + time.toString() + ' >> Welcome to TPS_BlueMagpie!</font>')
         self.command_message.setText(welcome_message)
         self.command_message.setReadOnly(True)
         #optional: string format:print('Hello, {:s}. You have {:d} messages.'.format('Rex', 999))
 
         # user input
         self.command_input= QLineEdit(self)
+        self.command_input.setFocusPolicy(Qt.StrongFocus)
+        self.command_input.setPlaceholderText("Type help to list commands ...")
         self.command_input.returnPressed.connect(self.send)
 
         # call history text
@@ -207,16 +264,7 @@ class Command(QWidget):
 
 
         # PyEPICS devices
-        self.hexapod_x = ped("hx", "41a:hexapod:x")
-
-        self.tsa = petd("tmp1", "41a:sample:tmp1")
-        self.tsb = petd("tmp2", "41a:sample:tmp2")
-
-        self.currentPV0 = "41a:sample:i0"
-        self.currentPV1 = "41a:sample:phdi"
-
-        self.AGM = e.Motor("41a:AGM:Energy")
-        self.AGS = e.Motor("41a:AGS:Energy")
+          # written but removed from this branch
 
     def send(self):
         text = self.command_input.text()
@@ -228,40 +276,72 @@ class Command(QWidget):
                           "history: Recall previously typed messages.\n"
                           "mv: Set a parameter to its absolute value.\n")
         elif text == "draw":
-            ImagingWidget.plot(self) # test communication
+            Y = np.linspace(-1.0, 1.0, 20)
+            self.v = Y
             self.command_message.append('<font color=blue>' + timestamp.toString() + ' >> ' + text + '</font>')
             return_text = ("Signal emitted.")
-            # Call Parameters
-        elif text == "x":
-            self.command_message.append('<font color=blue>' + timestamp.toString() + ' >> ' + text + '</font>')
-            return_text = (str(self.hexapod_x.getValue()))
-        elif text == "Tsa":
-            self.command_message.append('<font color=blue>' + timestamp.toString() + ' >> ' + text + '</font>')
-            return_text = (str(self.tsa.getValue()))
-        elif text == "Tsb":
-            self.command_message.append('<font color=blue>' + timestamp.toString() + ' >> ' + text + '</font>')
-            return_text = (str(self.tsb.getValue()))
-        elif text == "I01":
-            self.command_message.append('<font color=blue>' + timestamp.toString() + ' >> ' + text + '</font>')
-            return_text = (str(e.caget(self.currentPV0)))
-        elif text == "I02":
-            self.command_message.append('<font color=blue>' + timestamp.toString() + ' >> ' + text + '</font>')
-            return_text = (str(e.caget(self.currentPV1)))
-        elif text == "AGM":
-            self.command_message.append('<font color=blue>' + timestamp.toString() + ' >> ' + text + '</font>')
-            return_text = (str(self.AGM.get_position()))
 
-        # Call Commands
+            # Call Parameters
+              # written but removed from this branch
+
+            # Call Commands
+              # no in this branch
 
         else:
            return_text = ("Type 'help' to list commands")
-
-
+       
+        
+        
         #self.history_text = text
         self.command_message.append(return_text)
         self.command_input.setText("")
 
-if __name__ == '__main__':
+    @property
+    def v(self):
+        return self._v
+
+    @v.setter
+    def v(self, value):
+        self._v = value
+        self.valueChanged.emit(value)
+        print("new data emitted.")
+        
+
+
+class ImageWidget(QWidget):
+
+    def __init__(self, parent=None):
+        super(ImageWidget, self).__init__(parent=parent)
+        self.plotWidget = PlotWidget(self)
+        self.imgplot(self, Data.data)
+        self.layoutVertical = QVBoxLayout(self)
+        self.layoutVertical.addWidget(self.plotWidget)
+
+    @staticmethod
+    def imgplot(self, x):
+        self.plotWidget.plot(x)
+    
+    def make_data_connection(self, data_object):
+        data_object.valueChanged.connect(self.get_data)
+
+    @pyqtSlot(object)
+    def get_data(self, val):
+        self.imgplot(self, val)
+        print("new data plot")
+
+
+
+def main():
     app = QApplication(sys.argv)
     ex = RIXS()
+    cmd = Command()
+    cmd.command_input.setFocus()
+    img=ImageWidget()
+    dt=Data()
+    img.make_data_connection(dt)
+    dt.make_cmd_connection(cmd)
     sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
