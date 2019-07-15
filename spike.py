dcc681af9194caafd71f5fb27e66cc0ebde81ef1
JasonJungler <jasons10219@gmail.com>
2019-07-15 10:01:14 +0800

    new spike removal v3

--- spike.py
+++ spike.py
@@ -1,4 +1,4 @@
-########## spike removal, last edited July 12, 2019 by DJH
+########## spike removal, last edited July 14, 2019 by DJH
 # spikeRemoval removes spikes in a region of interest along x-pixel of an input 2D data array.
 # It provides 2D data array after spike removal
 #### input: data, x1 and x2, discrimination factor d
@@ -87,11 +87,12 @@ def spikeRemoval_1(data, x1=400, x2=600, d=3):
 
 def spikeRemoval(data, d=3): # the 3rd version
     # --- data is a 2D np.array
-# return list: [data_sp, peak_x, level]
+    # return list: [data_sp]
     # --- data_sp is a np.array for 2D data after spike removal
     # --- level is a np.array which stores the discriminator levels (the spike removal level) for all y pixels
     # --- level[j] = the data average of a given y-pixe of j with x pixels between x1 and x2.
-
+    if d<2:
+        d = 2
     size_y = np.size(data[0,:])
     size_x = np.size(data[:,0])
     level = np.copy(data[0,:]) # with y-pixel as the array index
@@ -99,56 +100,92 @@ def spikeRemoval(data, d=3): # the 3rd version
     for j in range(size_y): # size_y = total y-pixel
         data_x = np.copy(data[:,j]) #1D data array (x-pixel) before spike removal for y-pixel = j
         data_x_new = np.copy(data[:,j])
+        data_x_2nd = np.copy(data[:,j])
         avg0 = np.mean(data_x) #get a prelimary average before spike removal
-        #### just get an initial avg
+        #### get an initial avg
         data2x = np.copy(data[:,j]) #1D data array (x-pixel) before spike removal for y-pixel = j
         peak_x_0, _ = find_peaks(data2x, height=avg0*2)
-        data_x_0 = np.delete(data2x, peak_x_0) #1D data array after spike removal; its size has been reduced by np.size(peak_x))
-        avg_1 = np.mean(data_x_0) #average after a prelimary spike removal
+        avg_1 = avg0
+        if np.size(peak_x_0)>0:
+                data_x_0 = np.delete(data2x, peak_x_0)# remove the elements of data_x which have indexes of peak_x_0
+                avg_1 = np.mean(data_x_0) #average after a prelimary spike removal
 
-        ### remove spikes
         H = avg_1 *d
-        loop = 0
-        while loop < 20:
-            loop +=1
-            peak_x, _ = find_peaks(data_x_new, height=(H,np.inf), width=(1, 10), distance=20)
 
-            if len(peak_x) > 0:
-                print ("j = ", j, ', loop = ', loop)
-                print("number of peak_x = ", len(peak_x))
-                print("peak_x = ", peak_x)
+        ###### finding spkies of a given y pixel
+        peak_x, _ = find_peaks(data_x_new, height=(H,np.inf), width=(0.2, 6), distance=10)
+        width_half= peak_widths(data_x_new, peak_x, rel_height=0.5)  # indices of spikes with intenisty larger than "H"
+        peak_temp = np.array([])
 
-                width_half= peak_widths(data_x_new, peak_x, rel_height=0.5)  # indices of spikes with intenisty larger than "H"
-                width = width_half[0]
-                ww = np.copy(width_half[0])
-                print ('width_half =', width)
-                ww=np.round(ww)
-                ww = ww.astype(int)
-                #print ('ww =', ww)
-                n=0
-                for m in peak_x:
-                    m1 = max(0, m-50)
-                    m2 = min(m+50, size_x)
-                    avg = np.mean(data_x[m1:m2]) # average around the peak
-                    h0= data_x[m] - avg
-                    w = ww[n]
-                    if m > w and m < (size_x-w-1):
-                        x= np.linspace(m-w, m+w, num=2*w+1)
-                        data_sp= data_x_new[m-w:m+w+1]
-                        print("data before sp_removing", data_sp)
-                        data_sp = data_sp - gaussian_1d(x, m, width[n]/2.355, h0, avg)+avg
-                        data_sp[data_sp < avg]= avg
-                        data_x_new[m-w:m+w+1] =data_sp
-                        print("data after sp_removing", data_x_new[m-w:m+w+1])
-                    n +=1
-            else:
-                break
+        if np.size(peak_x) > 0:
+            #print("y-piexel = ", j, "number of peak_x = ", np.size(peak_x))
+            #print("peak_x = ", peak_x)
+            width = width_half[0]
+            #print ('width_half =', width)
+            ww = np.copy(width_half[0])
+            ww=np.round(ww)
+            ww = ww.astype(int)
+            #print ('ww =', ww)
+            peak_temp = np.copy(peak_x)
 
-        #now data_x_new are the data after spikle removal  and its size remains unchanged
-        level[j] = H  # a np.array which stores the discriminator level corresponding y pixels
+            n=0 # index for the spike found
+            for m in peak_temp:
+                #print(n+1,'-th point, x-piexel = ', peak_temp[n])
+
+                ## consider a 21-piexel range around the spike
+                m1 = max(0, m-10)
+                m2 = min(m+10, size_x)
+                avg = (21*np.mean(data_x_new[m1:m2])- data_x_new[m-1:m]-data_x_new[m:m+1]-data_x_new[m+1:m+2])/18# average around the peak
+                #print("peak index = ", m, ' peak intensity = ', data_x_new[m], '  avg =  ', avg, '   sp factor =', d)
+                h0= data_x_new[m] - avg
+
+                w = 7 ## a wondow for curve fit is set to 2w+1
+                if m > w and m < (size_x-w-1):
+                    x= np.linspace(m-w, m+w, num=2*w+1)
+                    data_sp= data_x_new[m-w:m+w+1]
+                    loop =0
+                    while 1:
+                        #peak_x_1, _ =find_peaks(data_sp, height=avg*d, width=(0.2, 4))
+                        #print('peaks found: ', peak_x_1)
+                        peak = np.max(data_sp[3:11])
+                        peak_index = np.argmax(data_sp[3:11])+m-5
+                        avg_2 = np.mean(data_sp)
+                        hh= peak - avg_2
+                        #print('peak= ', peak, 'avg_2= ', avg_2)
+                        if peak > 1.5* avg_2:
+                            print('loop = ', loop)
+                            print("data before sp_removing", data_sp.astype(int))
+                            # gaussian_1d(x, x0,sigma, H, c)
+                            initial_guess = (peak_index, width[n]/2.355, hh, avg_2)
+                            limit = ([m-3, 0.1, hh*0.5, avg_2*0.8],[m+3, 5, hh*2, avg_2*1.2])
+
+                            try:
+                                fit_x, pcov = curve_fit(gaussian_1d, x, data_sp, p0=initial_guess, bounds=limit)
+                                #print ("fit results:", gaussian_1d(x, fit_x[0], fit_x[1], fit_x[2], fit_x[3])+ fit_x[3])
+                                data_sp = data_sp - gaussian_1d(x, fit_x[0], fit_x[1], fit_x[2], fit_x[3])+ fit_x[3]
+                                for q in range(np.size(data_sp)):
+                                    if data_sp[q] < np.mean(data_sp)*0.9:
+                                        data_sp[q]= np.mean(data_sp)
+
+                                #print("after sp_removing", data_sp.astype(int))
+
+                            except RuntimeError:
+                                print("Error - curve_fit failed, using peak_widths")
+                                data_sp = data_sp - gaussian_1d(x, m, width[n]/2.355, hh, avg_2)+avg_2
+                        else:
+                            data_x_new[m-w:m+w+1] =np.copy(data_sp)
+                            break
+                        if loop > 10:
+                            data_x_new[m-w:m+w+1] =np.copy(data_sp)
+                            break
+                        loop +=1
+
+                    data_x_new[m-w:m+w+1] =np.copy(data_sp)
+                    data_int = data_sp.astype(int)
+                    #print("final data after sp_removing", data_int)
+                n +=1
         data[:,j] = data_x_new
-
-    return data
+    return [data]
 
 
 #### removing spikes from data aftre background subtartion
@@ -238,8 +275,6 @@ def gaussian_1d(x, x0,sigma, H, c):
    return H * np.exp( -0.5*((x-x0)/sigma)**2)+c
 
 
-
-
 def plot_spectrum(im_fft):
     # A logarithmic colormap
     plt.imshow(np.abs(im_fft), norm=LogNorm(vmin=5))
@@ -256,5 +291,5 @@ def plot_spectrum(im_fft):
 
 #data = np.reshape(data, (1024, 2048), order='F')
 
-#spikeRemoval_3(data)
+#spikeRemoval(data)
 #print('spike removal results:', spikeRemoval_3(data))
