dcda2e0fa4099c0a98876f2eacc159479300172c
JasonJungler <jasons10219@gmail.com>
2019-08-15 17:53:28 +0800

    New macro system
    (1) Macro Editor
    - called by "macro" command or "do macrofilename"
    - open/save allowed when there's no macro running
    - nothing allowed if macro running but Macro Monitor didn't ask for
    edition
    - resume/save allowed if macro running and Macro Monitor asked for
    edition
    (2) Macro Monitor
    - called by "do macrofilename" or already exist since last macro
    - if macro running, allowed to press "modify" to pause till current
    running command line.
    - if no macro running, nothing allowed.

---
+++ macro_editor.py
@@ -0,0 +1,168 @@
+import PyQt5
+from PyQt5 import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys, os
+
+class MacroEditor(QWidget):
+    errorMsg = pyqtSignal(str,str)
+    macroMsg = pyqtSignal(str)
+    windowclosed = pyqtSignal()
+    def __init__(self, directory, filepath=None):
+        super().__init__()
+        self.running = True if (filepath != None) else False
+        self.editing = True if (filepath != None) else False
+        self.current_file = filepath
+        self.directory = directory
+        self.__mainwindow()
+        self.__buttons()
+        self.__editor()
+        self.__layout()
+        self.__buttonmode()
+
+    def __mainwindow(self):
+        self.setWindowTitle("Macro Editor")
+        self.resize(400, 600)
+        self.setWindowFlags(self.windowFlags() & Qt.CustomizeWindowHint)
+        self.setWindowFlags(self.windowFlags() & ~Qt.WindowMinMaxButtonsHint)
+
+    def __editor(self):
+        self.editor = QTextEdit(self)
+        self.editor.setFocusPolicy(Qt.StrongFocus)
+        self.editor.setFocus()
+
+    def __buttons(self):
+        self.save_button = QPushButton("save", self)
+        self.open_button = QPushButton("open", self)
+        self.resume_button = QPushButton("resume", self)
+        self.save_button.clicked.connect(self.overWrite)    # check overwrite problem first, then decide new file name
+        self.open_button.clicked.connect(self.open)         # request user to choose a file
+        self.resume_button.clicked.connect(self.resume)     # activated while macro started
+        self.__buttonmode()
+        self.buttons_layout = QHBoxLayout()
+        self.buttons_layout.addWidget(self.save_button)
+        self.buttons_layout.addWidget(self.open_button)
+        self.buttons_layout.addWidget(self.resume_button)
+
+    def __layout(self):
+        self.window_layout = QVBoxLayout(self)
+        self.window_layout.addWidget(self.editor)
+        self.window_layout.addLayout(self.buttons_layout)
+
+    def __buttonmode(self):
+        self.open_button.setDisabled(self.running)
+        self.save_button.setDisabled(False if self.editing else self.running)
+        self.resume_button.setEnabled(True if self.editing else False)
+    
+    def __readFile(self):
+        filelist=[]
+        with open(self.current_file,"r") as f:
+            for x in f:
+                x = x.replace("\n", "")
+                filelist.append(x)
+        return filelist
+
+    def __editMode(self):
+        option = True if self.editing else False
+        self.editor.setReadOnly(not option)
+        self.editor.setStyleSheet("color: black; background-color: {}".format("white" if option else "Floralwhite"))
+
+    def overWrite(self):
+        if self.current_file != None: #if there's a name already, check overwrite
+            buttonReply = QMessageBox.question(self, 'Save Macro', "Do you want to overwrite?",
+                                               QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+            if buttonReply == QMessageBox.Yes:
+                self.save(os.path.basename(self.current_file))
+            else:
+                file_name = self.getName()
+                self.save(file_name)
+        else:
+            file_name = self.getName()
+            self.save(file_name)
+
+    def save(self, file_name):
+        if file_name != False:
+            text = self.editor.toPlainText()  # transform user_text to plaintext
+            txt_file = "{0}{1}".format(self.directory, file_name)
+            with open(txt_file, "w") as file:
+                file.write(text)
+            self.setWindowTitle("Macro Editor - {0}".format(file_name))
+
+    def getName(self, msg="", file_name = False):
+        text, ok = QInputDialog.getText(self, 'Save Macro', 'New macro name:\n'
+                                                            '{}'.format(msg))
+        if ok:
+            if text != "":
+                if not os.path.exists('{0}{1}.txt'.format(self.directory,text)):
+                    self.macroMsg.emit("macro file saved: {0}.txt".format(text))
+                    file_name = str(text)+'.txt'
+                else:
+                    error_msg = "File name already exist: {0}.".format(text)
+            else:
+                error_msg = 'file name can\'t be empty'
+        else:
+            error_msg = 'file not saved.'
+        if file_name == False:
+            self.errorMsg.emit(error_msg, 'err')
+            self.getName(error_msg)
+        return (file_name)
+
+    def open(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open macro file',
+                                            directory=self.directory
+                                            , filter='Text files (*.txt)'
+                                            , options=QFileDialog.ReadOnly)
+        if fname != ('', ''):
+            file = fname[0]
+            self.load(file)
+
+    def load(self, filepath = None):
+        if filepath != None:
+            with open(filepath, 'r') as f:
+                data = f.read()
+                self.editor.setText(data)
+                title = "Macro Editor - {0}".format(os.path.basename(f.name))
+                self.current_file = filepath
+        else:
+            title = "Macro Editor"
+        self.setWindowTitle(title)
+
+    def modify(self, current_index):
+        self.editing = True
+        filelist = self.__readFile()
+        self.editor.setText('<br>'.join(filelist[current_index+1:-1]))
+        self.__buttonmode()
+        self.__editMode()
+
+    def resume(self):
+        self.editing = False
+        filelist = self.__readFile()[:-1]
+        with open(self.current_file, "w") as f:
+            f.write('\n'.join(filelist))
+        self.__buttonmode()
+        self.__editMode()
+
+    def marcoStart(self, name):
+        self.running = True
+        self.__buttonmode()
+        self.load(name)
+
+    def macroFinished(self):
+        self.running = False
+        self.__buttonmode()
+        self.load(self.current_file)
+
+    def closeEvent(self, event):
+        if self.editing == False:
+            self.windowclosed.emit()
+            event.accept()
+        else:
+            self.errorMsg.emit('Can\'t close macro editor window while macro is running.', 'err')
+            event.ignore()
+
+# test running independently
+app = QApplication(sys.argv)
+macro = MacroEditor("a")
+macro.show()
+sys.exit(app.exec_())
---
+++ macro_monitor.py
@@ -0,0 +1,115 @@
+import PyQt5
+from PyQt5 import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys, os
+from os import path
+
+class MacroMonitor(QWidget):
+    '''
+    Popup a window to monitor macro running status.
+    '''
+    errorMsg = pyqtSignal(str,str)
+    macroMsg = pyqtSignal(str)
+    openEditor = pyqtSignal(int)
+    currentline = pyqtSignal(int)
+    windowclosed = pyqtSignal()
+
+    def __init__(self, filepath):
+        super().__init__()
+        self.filepath = filepath # inherited from Mainwindow
+        self.__mainwindow()
+        self.__layout()
+        self.__load()
+        self.running = True
+        
+    def __mainwindow(self):
+        self.setWindowTitle("Macro Monitor - {}".format(path.basename(self.filepath)))
+        self.resize(400, 600)
+        self.setWindowFlags(self.windowFlags() & Qt.CustomizeWindowHint)
+        self.setWindowFlags(self.windowFlags() & ~Qt.WindowMinMaxButtonsHint)
+
+    def __layout(self):
+        self.monitor = QTextEdit(self)
+        self.monitor.setReadOnly(True)
+        self.monitor.setFocusPolicy(Qt.StrongFocus)
+        self.monitor.setFocus()
+        self.monitor.setStyleSheet("color: black; background-color: Floralwhite")
+        self.modify_button = QPushButton("modify", self)
+        self.modify_button.clicked.connect(self.modify)  
+        self.window_layout = QVBoxLayout(self)
+        self.window_layout.addWidget(self.monitor)
+        self.window_layout.addWidget(self.modify_button)
+      
+    def __load(self):
+        try:
+            with open(self.filepath, 'r') as f:
+                data = f.read()
+                self.monitor.setText(data)
+        except:
+            self.errorMsg.emit('failed to load file {}'.format(self.filepath),'err')
+            print('failed to load file{}'.format(self.filepath))
+
+    def __readFile(self):
+        '''
+        Return command lines as list
+        '''
+        filelist=[]
+        with open(self.filepath,"r") as f:
+            for x in f:
+                x = x.replace("\n", "")
+                filelist.append(x)
+        return filelist
+
+    def modify(self):
+        '''
+        popup editor window for user modification, running macro will be paused
+        '''
+        if self.current_index != len(self.__readFile()):
+            with open(self.filepath, "a") as f:
+                f.write("\n###MacroPause###")       # for MainWindow to check pause, will be overwrited when new file is saved
+            self.openEditor.emit(self.current_index)
+
+    def update(self, macro_index):
+        '''
+        Receive index of the running command from Mainwindow QThread,
+        mark commands in different colors. 
+        '''
+        self.currnet_index = macro_index
+        filelist = self.__readFile()
+        if macro_index < len(filelist):         # macro_index = running line
+            line = filelist[macro_index]
+            for line in filelist[:macro_index]:
+                filelist[line] = '<font color = gray> {} </font>'.format(line)
+            self.monitor.setText('<br>'.join(filelist))
+
+    def closeEvent(self, event):
+        '''
+        Ignore close event while macro is running.
+        '''
+        if self.running == False:
+            self.windowclosed.emit()
+            event.accept()
+        else:
+            self.errorMsg.emit('Can\'t close macro monitor while macro is running.', 'err')
+            event.ignore()
+
+    def marcoStart(self, name):
+        '''
+        setup for macro monitor if window was already opened.
+        '''
+        self.running = True
+        self.filepath = name
+        self.__load()
+        self.modify_button.setEnabled(True)
+
+    def macroFinished(self):
+        self.running = False
+        self.modify_button.setDisabled(True)
+     
+# test running independently
+app = QApplication(sys.argv)
+macro = MacroMonitor("a")
+macro.show()
+sys.exit(app.exec_())
